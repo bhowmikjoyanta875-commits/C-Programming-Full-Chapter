1829e7ea5a951a7728ea22fd9547f72324c9d248
Joyanata <bhowmikjoyanta875@gmail.com>
2026-09-23 20:53:42 +0600

    array

--- chapter_7/array.c
+++ chapter_7/array.c
@@ -2,6 +2,21 @@
 
 int main()
 {
-    
+    int numbers[5];
+
+    printf("Enter 5 numbers:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("The numbers are:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", numbers[i]);
+    }
+
     return 0;
 }
